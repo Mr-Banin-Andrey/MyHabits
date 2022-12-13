5b737fd90fcff51aaaa9b8b9460c2bf0140ae4f3
Andrey Banin <109150204+Mr-Banin-Andrey@users.noreply.github.com>
2022-12-13 19:52:27 +0100

    Initial Commit

---
+++ MyHabits/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyHabits
+//
+//  Created by Андрей Банин on 13.12.22..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
